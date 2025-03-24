12266ba75500313aa732f2f2d72d030a0975ea22
Samonto Karmaker <samonto0726@gmail.com>
2025-03-24 19:01:28 +0600

    Add role service skeleton

---
+++ server/service/role.service.js
@@ -0,0 +1,9 @@
+const createCustomRoles = async (name, permissionIds) => {};
+
+const grantPermissionToRole = async () => {};
+
+const revokePermissionFromRole = async () => {};
+
+const getRolePermissions = async () => {};
+
+const getRoles = async () => {};
