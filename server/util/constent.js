ee195bc8f8d413f35ec63b988d48ad3a604ea179
Samanta Karmaker <samanta.karmaker@brainstation-23.com>
2025-03-24 13:25:08 +0600

    Add constants for Role, Priority, and TaskStatus

---
+++ server/util/constent.js
@@ -0,0 +1,17 @@
+export const Role = Object.freeze({
+  DEV: "DEV",
+  PROJECT_MANAGER: "PROJECT_MANAGER",
+  ADMIN: "ADMIN",
+});
+
+export const Priority = Object.freeze({
+  LOW: "LOW",
+  MEDIUM: "MEDIUM",
+  HIGH: "HIGH",
+});
+
+export const TaskStatus = Object.freeze({
+  PENDING: "PENDING",
+  IN_PROGRESS: "IN_PROGRESS",
+  COMPLETED: "COMPLETED",
+});
