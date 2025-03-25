7f1bd2a4f6a24848ef39c2fb98afc134f058bc1d
Samonto Karmaker <samonto0726@gmail.com>
2025-03-25 13:02:40 +0600

    Add authorization middleware to create custom roles endpoint

--- server/route/rolePermissionRoutes.js
+++ server/route/rolePermissionRoutes.js
@@ -4,11 +4,21 @@ import {
     getAllPermissionsController,
     getRolePermissionsController,
 } from "../controller/rolePermission.controller.js";
+import {
+    checkAuth,
+    checkRole,
+} from "../middleware/common/authorization.middleware.js";
+import { UserPermissions } from "../util/constant.js";
 
 const rolePermissionRouter = Router();
 
 rolePermissionRouter.get("/", getAllPermissionsController);
-rolePermissionRouter.post("/", createCustomRolesController);
+rolePermissionRouter.post(
+    "/",
+    checkAuth,
+    checkRole(UserPermissions.CREATE_ROLE),
+    createCustomRolesController
+);
 rolePermissionRouter.get("/:roleId", getRolePermissionsController);
 
 export default rolePermissionRouter;
