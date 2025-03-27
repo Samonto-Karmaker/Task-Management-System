54779e0bb4ea006721eef97bfdf7da66accd869e
Samonto Karmaker <samonto0726@gmail.com>
2025-03-27 17:47:16 +0600

    Add initial task service skeleton

---
+++ server/service/task.service.js
@@ -0,0 +1,8 @@
+export const createTask = async (task) => {};
+export const getTaskById = async (id) => {};
+export const updateTaskDetails = async (id, task) => {};
+export const deleteTask = async (id) => {};
+export const getAllTasks = async () => {};
+export const getTasksByAssigner = async (userId) => {};
+export const getTasksByAssignee = async (userId) => {};
+export const updateTaskStatus = async (id, status) => {};
