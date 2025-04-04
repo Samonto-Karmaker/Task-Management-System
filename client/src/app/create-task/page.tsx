3fa1918ea62c5a60b649d943cf074f76b289cbc1
Samonto Karmaker <samonto0726@gmail.com>
2025-04-04 14:40:45 +0600

    Add user assignment selection to create task form with API integration

--- client/src/app/create-task/page.tsx
+++ client/src/app/create-task/page.tsx
@@ -12,8 +12,44 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Priority } from "@/utils/constant";
+import { useEffect, useState } from "react";
+import { ApiResponse } from "@/types/api-response";
+import apiClient from "@/lib/apiClient";
+import { useRef } from "react";
+
+interface AssignAbleUser {
+    id: string;
+    name: string;
+    email: string;
+    role: string;
+}
 
 export default function CreateTaskForm() {
+    const [assignableUsers, setAssignableUsers] = useState<AssignAbleUser[]>([]);
+    const hasAssignableUsersFetched = useRef(false);
+
+    useEffect(() => {
+        const fetchAssignAbleUsers = async () => {
+            if (!hasAssignableUsersFetched.current) {
+                try {
+                    const response: ApiResponse = await apiClient.get(
+                        "/task/assignable-users"
+                    );
+                    if (response.success) {
+                        setAssignableUsers(response.data);
+                        hasAssignableUsersFetched.current = true; // Mark as fetched
+                    } else {
+                        console.error(response.message);
+                    }
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        };
+
+        fetchAssignAbleUsers();
+    }, []);
+
     return (
         <div className="max-w-md mx-auto">
             <Card>
@@ -24,13 +60,28 @@ export default function CreateTaskForm() {
                 </CardHeader>
                 <CardContent>
                     <form className="space-y-4">
+                    <div className="space-y-2">
+                            <Label>Select Assignee</Label>
+                            <Select>
+                                <SelectTrigger className="w-full">
+                                    <SelectValue placeholder="Select an Assignee" />
+                                </SelectTrigger>
+                                <SelectContent>
+                                    {assignableUsers.map((user) => (
+                                        <SelectItem key={user.id} value={String(user.id)}>
+                                            {user.name} - {user.email} - {user.role}
+                                        </SelectItem>
+                                    ))}
+                                </SelectContent>
+                            </Select>
+                        </div>
                         <div className="space-y-2">
                             <Label htmlFor="title">Title</Label>
                             <Input id="Title" placeholder="Task Title" />
                         </div>
 
                         <div className="space-y-2">
-                            <Label htmlFor="description">Email</Label>
+                            <Label htmlFor="description">Description</Label>
                             <Input
                                 type="text"
                                 id="description"
@@ -50,14 +101,11 @@ export default function CreateTaskForm() {
                             <Label>Select Priority</Label>
                             <Select>
                                 <SelectTrigger className="w-full">
-                                    <SelectValue placeholder="Select a role" />
+                                    <SelectValue placeholder="Select a priority" />
                                 </SelectTrigger>
                                 <SelectContent>
                                     {Object.values(Priority).map((p) => (
-                                        <SelectItem
-                                            key={p}
-                                            value={String(p)}
-                                        >
+                                        <SelectItem key={p} value={String(p)}>
                                             {p}
                                         </SelectItem>
                                     ))}
