012d982cba4e8bcf45f43a4f20161ab73260b121
Samonto Karmaker <samonto0726@gmail.com>
2025-04-07 14:09:18 +0600

    Add Loading component to standardize loading state display in AssignedTasks and CreatedTasks

--- client/src/app/created-tasks/page.tsx
+++ client/src/app/created-tasks/page.tsx
@@ -2,18 +2,14 @@
 
 import EmptyTable from "@/components/custom/EmptyTable";
 import { useState } from "react";
-import { Loader2 } from "lucide-react";
+import Loading from "@/components/custom/Loading";
 
 export default function AssignedTasks() {
     const [tasks, setTasks] = useState([]);
     const [loading, setLoading] = useState(true);
 
     if (loading) {
-        return (
-            <div className="flex items-center justify-center w-full h-full">
-                <Loader2 className="w-16 h-16 animate-spin text-gray-400" />
-            </div>
-        );
+        return <Loading />;
     }
     if (tasks.length === 0) {
         return <EmptyTable />;
