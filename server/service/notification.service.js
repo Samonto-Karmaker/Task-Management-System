cae58a431592aa55459183c3a079c5be5acd7ca7
Samonto Karmaker <samonto0726@gmail.com>
2025-04-09 11:22:27 +0600

    Add initial notification service functions for in-app and email notifications

---
+++ server/service/notification.service.js
@@ -0,0 +1,11 @@
+export const createInAppNotification = async (notification) => {};
+
+export const getInAppNotifications = async (userId) => {};
+
+export const markNotificationsAsRead = async (notificationIds) => {};
+
+export const sendInAppNotification = async (notificationId) => {};
+
+export const createEmailNotification = async (notification) => {};
+
+export const sendEmailNotification = async (notificationId) => {};
