af6613a5184499371fa7334611c0a83e82256f73
Samonto Karmaker <samonto0726@gmail.com>
2025-04-09 13:03:00 +0600

    Add notification routing for in-app notifications and unread count

---
+++ server/route/notificationRoute.js
@@ -0,0 +1,21 @@
+import { Router } from "express";
+import { checkAuth } from "../middleware/common/authorization.middleware.js";
+import {
+    getInAppNotificationsController,
+    getUnreadNotificationsCountController,
+} from "../controller/notification.controller";
+
+const notificationRouter = Router();
+notificationRouter.use(checkAuth);
+
+notificationRouter.get(
+    "/in-app-notifications",
+    getInAppNotificationsController
+);
+
+notificationRouter.get(
+    "/unread-notifications-count",
+    getUnreadNotificationsCountController
+);
+
+export default notificationRouter;
--- server/route/setupRoutes.js
+++ server/route/setupRoutes.js
@@ -1,5 +1,6 @@
 import { BASE_URL } from "../util/constant.js";
 import analyticsRouter from "./analyticsRoute.js";
+import notificationRouter from "./notificationRoute.js";
 import rolePermissionRouter from "./rolePermissionRoute.js";
 import taskRouter from "./taskRoute.js";
 import testRouter from "./testRoute.js";
@@ -11,4 +12,5 @@ export const setupRoutes = (app) => {
     app.use(`${BASE_URL}/role-permission`, rolePermissionRouter);
     app.use(`${BASE_URL}/task`, taskRouter);
     app.use(`${BASE_URL}/analytics`, analyticsRouter);
-}
+    app.use(`${BASE_URL}/notification`, notificationRouter);
+};
